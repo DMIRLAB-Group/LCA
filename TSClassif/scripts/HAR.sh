b456aed414740b378fb9bb0589e5b8e973561391
Junxian Huang <huangjunxian459@gmail.com>
2025-01-02 16:58:47 +0800

    first commit
    The code of time series classification.

---
+++ TSClassif/scripts/HAR.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run.py  --save_dir LCA_all_result --exp_name HAR_type1 --da_method LCA --dataset HAR --num_run 3  --type type1 --lr 0.001
---
+++ TSClassif/scripts/HHAR_SA.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run.py  --save_dir LCA_all_result --exp_name HHAR_SA_type1 --da_method LCA --dataset HHAR_SA --num_run 3  --type type1 --lr 0.001
